7a9fe0b28136255bc788daa4e91225dc9a898989
Charles Lamb <charles.lamb.dev@gmail.com>
2025-10-22 18:04:01 -0400

    imported ioexcepion and added a song to the server

--- backend/src/main/java/com/unis/service/MediaService.java
+++ backend/src/main/java/com/unis/service/MediaService.java
@@ -19,6 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
