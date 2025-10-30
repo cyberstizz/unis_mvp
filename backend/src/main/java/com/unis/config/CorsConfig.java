f83febe065f29b29d33c5b2c827ffb2dc98fb16c
Charles Lamb <charles.lamb.dev@gmail.com>
2025-10-30 14:53:00 -0400

    fixed application.yml indentation issue and added CorsConfig for freontend testing

---
+++ backend/src/main/java/com/unis/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.unis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("http://localhost:3000"); // Vite dev
+        config.addAllowedOriginPattern("http://127.0.0.1:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
+}
